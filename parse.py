0a011aee6f63a1e38b038f057cccc6ffc53f9af2
Michelle Olynyk class of 2016 <molynyk@tempest.wellesley.edu>
2016-04-30 13:58:39 -0400

    Fixed bugs in parse.py, should be ready to go, if running through the commandline takes in the input file name as an argument and the max amount of sentences to be returned. If running through another program, use the main() method which takes the input file name, and the max amount of sentences to be returned as arguments. Again, same comment of needing to manually edit the sampleProps.properties file with the new input file name if you're changing the file each time. Otherwise can just leave it and overwrite.

--- parse.py
+++ parse.py
@@ -1,41 +1,48 @@
-from nltk.parse.bllip import BllipParser
-from nltk.tokenize import word_tokenize
-from nltk import pos_tag 
-from nltk.data import find
-from nltk.parse import stanford
-#import bllipparser
+import os
+import sys
+import subprocess
 
-#initializes parser using bllip parsing model described by nltk parsers api
-#allows parsing of sentences with or without pos_tags 
-def initialize_parser():
-    model_dir = find('models/bllip_wsj_no_aux').path
-    print('Loading BLLIP Parsing models...')
+#Written by Misha Olynyk
 
-    bllip = BllipParser.from_unified_model_dir(model_dir)
-    print('Done.')
-    return bllip
+'''
+Creates a subprocess that runs the stanford nlp, putting the output into 
+an output file which includes a parse tree.
+WARNING, BEFORE RUNNING BE SURE THAT sampleProps.properties REFLECTS THE
+INPUT FILE NAME.
+'''
+def run_parser(f):
+    args = ['java','-Xmx1g','edu.stanford.nlp.pipeline.StanfordCoreNLP','-props','sampleProps.properties']
+    p = subprocess.Popen(args)
+    if os.path.isfile(f + '.output'):
+        p.terminate() #after results saved to file terminate the subprocess
+'''
+Analyze the output file, specifically the returned parse tree. And return
+five non-fragment sentences by determining whether an S is present after ROOT
+'''
+def analyze_results(f_name,num_sents):
+    f = open(f_name)
+    output = open(f_name + '.output')
+    input_lines = f.readlines()
+    complete_sentences = []
+    input_index = 0
+    print "I'm in this method"
+    for line in output.readlines():
+        if "<parse>" in line:
+            if '(ROOT (S' in line:
+                sentence = input_lines[input_index]
+                complete_sentences.append(sentence[:len(sentence)-1])
+            input_index += 1
+            if len(complete_sentences) == num_sents:
+                break
+    return complete_sentences
 
-#more pseudocode than anything, depends what parsed sentences look like
-#if stanford parser then a tree format and have to learn how to iterate through trees.
-def is_complete(parser,sentence):
-    output = parser.parse(sentence)
-    return output[0] == 'S'
+def main(f_name,sents):
+    run_parser(f_name)
+    return analyze_results(f_name,sents)
 
-def parse_sentences(parser, sentences):
-    result = []
-    incomplete_sentence = ''
-    for s in sentences:
-        if is_complete(parser,s):
-            result.append(s)
-        #dependent on how the tree works and how to look through it, more complicated, above could potentially work
-        #if on a longer time frame.
-        else:
-            if incomplete_sentence == '':
-                incomplete_sentence = s
-            else:
-                incomplete_parse = parser.parse(incomplete_sentence) 
-                new_parse = parser.parse(s)
-
-
-if __name__ == 'main':
-    #parser = initialize_parser()
+if __name__ == '__main__':
+    f_name = sys.argv[1]
+    sents = int(sys.argv[2])
+    run_parser(f_name)
+    complete_sentences = analyze_results(f_name,sents)
+    print complete_sentences
